1f01b017ae8fb507d9b6fbd55494d3be5943d6f0
Yomna khaled <yomnakhaled2001@gmail.com>
2024-06-03 09:42:39 +0300

    update home page

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,6 +1,18 @@
+import Home_Header from "../Components/homeComponents/home_header";
+import HomeAbout from "../Components/homeComponents/home_about";
+import HomeCounter from "../Components/homeComponents/home_counter";
+import HomeDepartment from "../Components/homeComponents/home_departments";
+import HomeServices from "../Components/homeComponents/homw_services";
+
 function Home () {
     return (
       <>
+      <Home_Header/>
+      <HomeAbout/>
+      <HomeCounter/>
+      <HomeDepartment/>
+      <HomeServices/>
+      
       </>
     );
   };
