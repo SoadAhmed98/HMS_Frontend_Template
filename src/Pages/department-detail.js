2934200063ba88317bc1f21f9e7929e241788fd1
Yomna khaled <yomnakhaled2001@gmail.com>
2024-06-11 20:50:32 +0300

    update the department detail page to take id from param

--- src/Pages/department-detail.js
+++ src/Pages/department-detail.js
@@ -6,8 +6,12 @@ import DoctorSec from "../Components/doctorSec.js";
 import  Navbar  from "../Components/navbar.js";
 import { useEffect } from "react";
 import WOW from 'wowjs';
+import { useParams } from "react-router-dom/cjs/react-router-dom.min.js";
 
 function DepartmentDetails() {
+
+  const param = useParams();
+  const id = param.id
 
   useEffect(() => {
     new WOW.WOW().init();
@@ -17,8 +21,8 @@ function DepartmentDetails() {
     <>
     <Navbar/>
       <Header head="Departments Of MediTech" content="What We Actually Do?" place="Departments" />
-      <DepartmentPage></DepartmentPage>
-      <DoctorSec></DoctorSec>
+      <DepartmentPage id={id} />
+      <DoctorSec/>
     </>
   );
 };
