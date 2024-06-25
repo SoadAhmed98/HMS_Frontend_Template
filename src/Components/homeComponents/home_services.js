486cded20791d41c3a5eb131156e8526faa019ae
Yasmeen Mohamed <135316122+YasmeenMohamed1@users.noreply.github.com>
2024-06-25 14:30:05 +0300

    Update home_services.js

--- src/Components/homeComponents/home_services.js
+++ src/Components/homeComponents/home_services.js
@@ -1,123 +1,86 @@
-import { useEffect } from "react";
-import { Link } from "react-router-dom/cjs/react-router-dom.min";
-// import WOW from 'wowjs';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
-function HomeServices () {
+function HomeServices() {
+    const [services, setServices] = useState([]);
 
-	// useEffect(() => {
-	// 	new WOW.WOW().init();
-	// }, []);
-    return (
-        <section class="services-section">
-		<div class="auto-container">
-			
-			
-			<div class="sec-title centered">
-				<h2>Best Health Services</h2>
-				<div class="separator"></div>
-			</div>
-			
-			<div class="row clearfix">
-			
-				{/* <!-- Left Column --> */}
-				<div class="left-column pull-left col-lg-4 col-md-12 col-sm-12">
-					<div class="inner-column">
-						
-						
-						<div class="service-block">
-							<div class="inner-box wow fadeInLeft" data-wow-delay="0ms" data-wow-duration="1500ms">
-								<div class="icon-box">
-									<span class="icon flaticon-doctor-stethoscope"></span>
-								</div>
-								<h3><Link to="#">Outdoor Checkup</Link></h3>
-								<div class="text">We provide best service for our cline. <br/> Now place take it.</div>
-							</div>
-						</div>
-						
-						
-						<div class="service-block">
-							<div class="inner-box wow fadeInLeft" data-wow-delay="250ms" data-wow-duration="1500ms">
-								<div class="icon-box">
-									<span class="icon flaticon-operating-room"></span>
-								</div>
-								<h3><Link to="#">Operation Theater</Link></h3>
-								<div class="text">We provide best service for our cline. Now place take it.</div>
-							</div>
-						</div>
-						
-						
-						<div class="service-block">
-							<div class="inner-box wow fadeInLeft" data-wow-delay="500ms" data-wow-duration="1500ms">
-								<div class="icon-box">
-									<span class="icon flaticon-van"></span>
-								</div>
-								<h3><Link to="#">Emergency Care</Link></h3>
-								<div class="text">We provide best service for our cline. <br/> Now place take it.</div>
-							</div>
-						</div>
-						
-					</div>
-				</div>
-				
-				
-				<div class="circles-column col-lg-4 col-md-12 col-sm-12">
-					<div class="inner-column">
-						
-						<div class="circles">
-							<div class="circle-one"></div>
-							<div class="circle-two"></div>
-							<div class="circle-three"></div>
-						</div>
-						
-					</div>
-				</div>
-				
-				
-				<div class="right-column pull-right col-lg-4 col-md-12 col-sm-12">
-					<div class="inner-column">
-						
-						
-						<div class="service-block-two">
-							<div class="inner-box wow fadeInRight" data-wow-delay="0ms" data-wow-duration="1500ms">
-								<div class="icon-box">
-									<span class="icon flaticon-water"></span>
-								</div>
-								<h3><Link to="#">Blood Test</Link></h3>
-								<div class="text">We provide best service for our cline. <br/> Now place take it.</div>
-							</div>
-						</div>
-						
-						
-						<div class="service-block-two">
-							<div class="inner-box wow fadeInRight" data-wow-delay="250ms" data-wow-duration="1500ms">
-								<div class="icon-box">
-									<span class="icon flaticon-pharmacy"></span>
-								</div>
-								<h3><Link to="#">Pharmacy Support</Link></h3>
-								<div class="text">We provide best service for our cline. Now place take it.</div>
-							</div>
-						</div>
-						
-						
-						<div class="service-block-two">
-							<div class="inner-box wow fadeInRight" data-wow-delay="500ms" data-wow-duration="1500ms">
-								<div class="icon-box">
-									<span class="icon flaticon-nurse"></span>
-								</div>
-								<h3><Link to="#">24/7 Service</Link></h3>
-								<div class="text">We provide best service for our cline. <br/> Now place take it.</div>
-							</div>
-						</div>
-						
-					</div>
-				</div>
-			
-			</div>
-			
-		</div>
-	</section>
-      
-    );
-  };
+    useEffect(() => {
+        // Fetch data from API
+        axios.get(`https://b43c1a73-82c8-4103-8569-c1e7e6a160cd.mock.pstmn.io/services`)
+            .then(res => {
+                setServices(res.data);
+            })
+            .catch(err => console.log(err));
+    }, []);
+
   
-  export default HomeServices;
+    const icons = [
+        'flaticon-doctor-stethoscope',
+        'flaticon-operating-room',
+        'flaticon-van',
+        'flaticon-water',
+        'flaticon-pharmacy',
+        'flaticon-nurse'
+    ];
+
+    return (
+        <section className="services-section">
+            <div className="auto-container">
+                <div className="sec-title centered">
+                    <h2>Best Health Services</h2>
+                    <div className="separator"></div>
+                </div>
+
+                <div className="row clearfix">
+                    {/* Left Column */}
+                    <div className="left-column pull-left col-lg-4 col-md-12 col-sm-12">
+                        <div className="inner-column">
+                            {services.slice(0, 3).map((service, index) => (
+                                <div key={index} className="service-block">
+                                    <div className="inner-box wow fadeInLeft" data-wow-delay={`${index * 250}ms`} data-wow-duration="1500ms">
+                                        <div className="icon-box">
+                                            <span className={`icon ${icons[index]}`}></span>
+                                        </div>
+                                        <h3><Link to="#">{service.name}</Link></h3>
+                                        <div className="text">{service.description}<br/> Now place take it.</div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+
+                    {/* Circles Column */}
+                    <div className="circles-column col-lg-4 col-md-12 col-sm-12">
+                        <div className="inner-column">
+                            <div className="circles">
+                                <div className="circle-one"></div>
+                                <div className="circle-two"></div>
+                                <div className="circle-three"></div>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Right Column */}
+                    <div className="right-column pull-right col-lg-4 col-md-12 col-sm-12">
+                        <div className="inner-column">
+                            {services.slice(3, 6).map((service, index) => (
+                                <div key={index} className="service-block-two">
+                                    <div className="inner-box wow fadeInRight" data-wow-delay={`${index * 250}ms`} data-wow-duration="1500ms">
+                                        <div className="icon-box">
+                                            <span className={`icon ${icons[index + 3]}`}></span>
+                                        </div>
+                                        <h3><Link to="#">{service.name}</Link></h3>
+                                        <div className="text">{service.description}<br/> Now place take it.</div>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default HomeServices;
